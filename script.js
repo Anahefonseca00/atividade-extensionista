2767de5476421202d7c7fa95d04d16413a504ee0
Anahefonseca00 <nasouzana@gmail.com>
2026-09-17 20:17:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.textContent = "Parabéns! Você criou sua primeira interação com JavaScript! 🎉";
+
+}
